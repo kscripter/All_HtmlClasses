cadda60b7b914ee438a975172fe8e1e76fdab953
Kaleem Habeeb <kalimhabeeb@gmail.com>
2021-03-12 17:57:33 +0100

    AllMyHTMLClasses

---
+++ app.js
@@ -0,0 +1,19 @@
+
+
+//window.prompt("Enter your name", "defaultText");
+
+
+//document.getElementById("p").style.color = "red";
+
+
+document.getElementById("p").click(function () {
+    
+});
+
+
+$("p").click(
+    function () {
+        $(this).hide();
+    })
+
+
